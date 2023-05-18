6247de8119e89da2ee4d26f87d8e7ebf19ca08a1
Jason Hyde <github@2bad.me>
2023-05-18 05:32:45 +0200

    [ts] add generic helpers for array methods

---
+++ source/utils/array.ts
@@ -0,0 +1,3 @@
+export const sortAsc = (a: number, b: number): number => a - b
+export const sortDesc = (a: number, b: number): number => b - a
+export const sum = (accumulator: number, value: number): number => accumulator + value
