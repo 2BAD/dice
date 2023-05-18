92bef115264eaccbf87ca9ffb1dca4290d0f525b
Jason Hyde <github@2bad.me>
2023-05-18 07:08:59 +0200

    [type] add `RollResult` type

--- source/type.ts
+++ source/type.ts
@@ -4,3 +4,11 @@ export type RollSpec = {
   modifier: number
   separator: 'd' | 'z'
 }
+
+export type RollResult = {
+  values: number[]
+  sum: number
+  min: number
+  max: number
+  isCritical: boolean
+}
