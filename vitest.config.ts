47b93e307dd3cfd664423d4f7bf9da6038f2935e
Jason Hyde <github@2bad.me>
2024-05-29 02:25:49 +0200

    [conf] update configurations from template repo

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,17 +1,13 @@
+import tsconfigPaths from 'vite-tsconfig-paths'
 import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
-    globals: true,
     exclude: ['build', 'node_modules'],
     coverage: {
-      provider: 'c8'
+      provider: 'v8'
     },
     testTimeout: 30000
   },
-  resolve: {
-    alias: {
-      '~': './source'
-    }
-  }
+  plugins: [tsconfigPaths()]
 })
